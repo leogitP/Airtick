b53c57a327a19a522c1c784f8dcb14c6e37e9c72
leogitD <leoaaa5566@gmail.com>
2023-02-10 16:03:31 +0800

    page

--- src/App.js
+++ src/App.js
@@ -9,21 +9,8 @@ import "jquery/dist/jquery.min.js";
 
 // 元件
 import Error from './components/Error';
-// 首頁
-import Home from './page/index/Home';
 // 機票
 import Airticket from './page/airticket/Airticket';
-// 景點
-import Spot from './page/spot/Spot';
-// 飯店
-import Hotel from './page/hotel/Hotel';
-// 行程表
-import Schedule from './page/schedule/Schedule';
-// 會員中心
-import MemberFavorite from './page/member/MemberFavorite';
-import MemberSchedule from './page/member/MemberSchedule';
-
-
 // 導覽列
 import Navigation from './components/tool/Navigation';
 
@@ -42,15 +29,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Airticket/>} exact />
-          <Route path="/Home" element={<Home/>} exact />
           <Route path="/Airticket" element={<Airticket/>} exact />
-          <Route path="/Spot" element={<Spot/>} />
-          <Route path="/Spot/:id" element={<Spot/>} />
-          <Route path="/Hotel" element={<Hotel/>} />
-          <Route path="/Hotel/:id" element={<Hotel/>} />
-          <Route path="/Schedule" element={<Schedule/>} />
-          <Route path="/member/MemberFavorite" element={<MemberFavorite/>} />
-          <Route path="/member/MemberSchedule" element={<MemberSchedule/>} />
           <Route component={Error} />
         </Routes>
       </BrowserRouter>
